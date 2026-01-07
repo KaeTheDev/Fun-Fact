a0c2f0924e7b99c0412d18a1800c8a7553f0bea1
KaeTheDev <kaethedev@gmail.com>
2026-01-07 17:06:11 -0500

    Added Node.js 'path' module

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // 1. import the express library
 const express = require('express');
 
+// Node.js 'path' module
+const path = require('path');
+
 // 2. Create an instance of an Express application
 const app = express();
 
